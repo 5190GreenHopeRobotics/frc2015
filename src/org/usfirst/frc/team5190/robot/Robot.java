785b3c323dda2c48151c4a37d321dfd566da821a
Samuel Jin <samfunkit@gmail.com>
2015-01-16 19:35:44 -0500

    Repositioned Subsystems into Robot.

--- src/org/usfirst/frc/team5190/robot/Robot.java
+++ src/org/usfirst/frc/team5190/robot/Robot.java
@@ -1,6 +1,7 @@
 
 package org.usfirst.frc.team5190.robot;
 
+import org.usfirst.frc.team5190.robot.subsystems.ArmSubsystem;
 import org.usfirst.frc.team5190.robot.subsystems.DriveTrainSubsystem;
 
 import edu.wpi.first.wpilibj.CameraServer;
@@ -10,14 +11,12 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
 /**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
+ *
  */
 public class Robot extends IterativeRobot {
-
+	
+	public static ArmSubsystem armSubsystem = new ArmSubsystem();
+	
 	public static OI oi;
 	public static final DriveTrainSubsystem driveTrain = new DriveTrainSubsystem();
 
