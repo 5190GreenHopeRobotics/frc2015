b2f094536a2e5cb7cc3f70ccd787be43d9a70124
Samuel Jin <samfunkit@gmail.com>
2015-01-16 19:23:49 -0500

    Rename

--- src/org/usfirst/frc/team5190/robot/commands/OpenGrabberCommand.java
+++ src/org/usfirst/frc/team5190/robot/commands/OpenGrabberCommand.java
@@ -5,9 +5,9 @@ import edu.wpi.first.wpilibj.command.Command;
 /**
  *
  */
-public class Opengrabber extends Command {
+public class OpenGrabberCommand extends Command {
 
-    public Opengrabber() {
+    public OpenGrabberCommand() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     }
