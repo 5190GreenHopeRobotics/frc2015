4e5c603b6c852e7bdf4447a84cf54ff7bd516110
Samuel Jin <samfunkit@gmail.com>
2015-01-16 19:33:04 -0500

    RobotMap Subsystem for ArmSubsystem

--- src/org/usfirst/frc/team5190/robot/RobotMap.java
+++ src/org/usfirst/frc/team5190/robot/RobotMap.java
@@ -1,8 +1,12 @@
 package org.usfirst.frc.team5190.robot;
 
+import org.usfirst.frc.team5190.robot.subsystems.ArmSubsystem;
+
 public class RobotMap {
 	public static final int GRABBER_SOLENOID_PORT = 1;
     public static final int ARM_SOLENOID_PORT = 0;
     public static final int ROBOT_DRIVE_PORT1 = 0;
     public static final int ROBOT_DRIVE_PORT2 = 1;
+    
+    public static ArmSubsystem armSubsystem = new ArmSubsystem();
 }
