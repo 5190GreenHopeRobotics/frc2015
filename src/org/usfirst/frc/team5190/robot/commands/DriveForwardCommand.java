192679985b3d63836a7d7f627b1ec40be0ec2ef7
Shilong Dai <sdai@viperfish.net>
2015-01-17 01:13:20 -0500

    added the drive forward command

---
+++ src/org/usfirst/frc/team5190/robot/commands/DriveForwardCommand.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team5190.robot.commands;
+
+import org.usfirst.frc.team5190.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveForwardCommand extends Command {
+
+    public DriveForwardCommand() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.driveTrain);
+    	setTimeout(2);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+   /**
+    * drive at full speed forward for 2 second.
+    */
+    protected void execute() {
+    	Robot.driveTrain.drive(1);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
