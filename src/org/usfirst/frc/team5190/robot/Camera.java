e9bd529257943534b9def8e10564342bf8f58017
rd124p7 <rd124p7@yahoo.com>
2015-01-17 10:32:24 -0500

    Updated Pop-Tarts

--- src/org/usfirst/frc/team5190/robot/Camera.java
+++ src/org/usfirst/frc/team5190/robot/Camera.java
@@ -4,25 +4,61 @@ package org.usfirst.frc.team5190.robot;
 
 
 
-//Import Camera 
+//Import Camera and Things
+import com.ni.vision.NIVision;
+import com.ni.vision.NIVision.DrawMode;
+import com.ni.vision.NIVision.Image;
+import com.ni.vision.NIVision.ShapeMode;
 import edu.wpi.first.wpilibj.CameraServer;
+
 
 
 //Public Camera Class
 public class Camera {
 	
-	CameraServer serv;
+	public Robot robot = new Robot();
+	
+	//Define Some Variables
+	int cameraSession;
+	Image cameraFrame;
 	
 	
-	{
-
-    serv = CameraServer.getInstance();
-    serv.setQuality(50);
-    serv.startAutomaticCapture("cam0");
+	public void cameraInit(){
+		
+		cameraFrame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
+		
+		
+		cameraSession = NIVision.IMAQdxOpenCamera("cam0", 
+				NIVision.IMAQdxCameraControlMode.CameraControlModeController);
+		
+		
+		NIVision.IMAQdxConfigureGrab(cameraSession);
 		
 	}
 	
-
+	public void cameraControl(){
+		
+		NIVision.IMAQdxStartAcquisition(cameraSession);
+		
+		
+		NIVision.Rect rect = new NIVision.Rect(10, 10, 100, 100);
+		
+		
+		while(robot.RobotIsEnabled){
+			
+			
+			NIVision.IMAQdxGrab(cameraSession, cameraFrame, 1);
+			
+			NIVision.imaqDrawShapeOnImage(cameraFrame, cameraFrame, rect, 
+					DrawMode.DRAW_VALUE, ShapeMode.SHAPE_OVAL, 0.0f);
+			
+			CameraServer.getInstance().setImage(cameraFrame);
+			
+		}
+		
+		NIVision.IMAQdxStopAcquisition(cameraSession);
+		
+	}
 
 
 }
