66941258766d2366c632c103f5078dca31fb9c2c
Shilong Dai <sdai@viperfish.net>
2015-01-17 01:24:30 -0500

    fixed the isFinished in the DriveForward

--- src/org/usfirst/frc/team5190/robot/commands/DriveForwardCommand.java
+++ src/org/usfirst/frc/team5190/robot/commands/DriveForwardCommand.java
@@ -29,7 +29,7 @@ public class DriveForwardCommand extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return isTimedOut();
     }
 
     // Called once after isFinished returns true
