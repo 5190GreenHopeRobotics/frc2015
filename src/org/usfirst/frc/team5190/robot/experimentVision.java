068130fc9ae632091e8e18b3c83cbd410a4c0c58
rd124p7 <rd124p7@yahoo.com>
2015-01-18 10:50:37 -0500

    New Robot Vision Class For Tracking

---
+++ src/org/usfirst/frc/team5190/robot/experimentVision.java
@@ -0,0 +1,36 @@
+package org.usfirst.frc.team5190.robot;
+
+public class experimentVision {
+	
+	
+	//Display Resolution 
+	final int IMAGE_RES = 480;
+	//Viewing 
+	final double VIEW_ANGLE = 68.5;
+	
+	// The Value of PI
+	final double PI = 3.141592653;
+	
+	
+	// Limits for Identification 
+	final int RECT_LIMIT = 40;
+	final int ASPECT_RATIO_LIMIT = 55;
+	
+	
+	// Limits for Target Identification
+	final int TAPE_WIDTH_LIMIT = 50;
+	final int VERTICAL_SCORE_LIMIT = 50;
+	final int SCORE_LIMIT = 50;
+	
+	// Minimum Area of particles 
+	final int AREA_MIN = 150;
+	
+	// Max of Particles 
+	final int MAX_PART = 8;
+	
+	
+	
+	
+	
+	
+}
