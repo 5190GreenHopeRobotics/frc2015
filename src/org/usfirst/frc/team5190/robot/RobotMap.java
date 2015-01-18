6485c984bfde15cf3842cd054a2f5a54fe951704
Shilong Dai <sdai@viperfish.net>
2015-01-18 15:22:17 -0500

    Support 4 motor now

--- src/org/usfirst/frc/team5190/robot/RobotMap.java
+++ src/org/usfirst/frc/team5190/robot/RobotMap.java
@@ -6,5 +6,6 @@ public class RobotMap {
     public static final int ARM_SOLENOID_PORT = 0;
     public static final int ROBOT_DRIVE_PORT1 = 0;
     public static final int ROBOT_DRIVE_PORT2 = 1;
-    
+    public static final int ROBOT_DRIVE_PORT3 = 2;
+    public static final int ROBOT_DRIVE_PORT4 = 3;
 }
