b0eb9fdcd2ad42ce1d173921b307816384f0d3da
Tony Zhang <ghidorah45@gmail.com>
2015-01-19 17:51:46 -0500

    Added raise and lower methods to forklift

--- src/org/usfirst/frc/team5190/robot/RobotMap.java
+++ src/org/usfirst/frc/team5190/robot/RobotMap.java
@@ -1,13 +1,13 @@
 package org.usfirst.frc.team5190.robot;
 
-
 public class RobotMap {
-    public static final int ARMLENGTH_TALONSRX_PORT = 2;
-    public static final int ARMANGLE_TALONSRX_PORT = 3;
-	public static final int PAW_TALONSRX_PORT = 4;
-    public static final int FRONTLEFT = 0;
-    public static final int BACKLEFT = 1;
-    public static final int FRONTRIGHT = 2;
-    public static final int BACKRIGHT = 3;
-    public static final int GYRO_PORT = 0;
+	public static final int ARMLENGTH_TALONSRX_PORT = 2;
+	public static final int ARMANGLE_TALONSRX_PORT = 3;
+	public static final int PAWOPEN_TALONSRX_PORT = 4;
+	public static final int PAWRAISE_TALONSRX_PORT = 5;
+	public static final int FRONTLEFT = 0;
+	public static final int BACKLEFT = 1;
+	public static final int FRONTRIGHT = 2;
+	public static final int BACKRIGHT = 3;
+	public static final int GYRO_PORT = 0;
 }
--- src/org/usfirst/frc/team5190/robot/subsystems/RobotPawSubsystem.java
+++ src/org/usfirst/frc/team5190/robot/subsystems/RobotPawSubsystem.java
@@ -1,16 +1,19 @@
 package org.usfirst.frc.team5190.robot.subsystems;
 
+import org.usfirst.frc.team5190.robot.RobotMap;
+
 import edu.wpi.first.wpilibj.TalonSRX;
 import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import org.usfirst.frc.team5190.robot.RobotMap;
 
 /**
  *
  */
 public class RobotPawSubsystem extends Subsystem {
-	private TalonSRX pawTalon = new TalonSRX(RobotMap.PAW_TALONSRX_PORT);
-	public Ultrasonic pawUltrasonic = new Ultrasonic(1,1);
+	private TalonSRX pawopenTalon = new TalonSRX(RobotMap.PAWOPEN_TALONSRX_PORT);
+	private TalonSRX pawraiseTalon = new TalonSRX(
+			RobotMap.PAWRAISE_TALONSRX_PORT);
+	public Ultrasonic pawUltrasonic = new Ultrasonic(1, 1);
 
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
@@ -18,23 +21,36 @@ public class RobotPawSubsystem extends Subsystem {
 		// Set the default command for a subsystem here.
 		// setDefaultCommand(new MySpecialCommand());
 	}
-	
+
 	public RobotPawSubsystem() {
 		pawUltrasonic.setEnabled(true);
 		pawUltrasonic.setAutomaticMode(true);
 	}
 
-	// Opens the grabber. Sets the speed 
+	// Opens the grabber. Sets the speed
 	public void openGrabber() {
-			pawTalon.set(1);
+		pawopenTalon.set(1);
 	}
+
 	// Close the grabber
 	public void closeGrabber() {
-			pawTalon.set(-1);
+		pawopenTalon.set(-1);
 	}
-	
+
 	// Stop the grabber
-	public void stopGrabber() {
-			pawTalon.set(0);
+	public void stopopenGrabber() {
+		pawopenTalon.set(0);
+	}
+
+	public void raiseGrabber() {
+		pawraiseTalon.set(1);
+	}
+
+	public void lowerGrabber() {
+		pawraiseTalon.set(1);
+	}
+
+	public void stopraiseGrabber() {
+		pawraiseTalon.set(0);
 	}
 }
