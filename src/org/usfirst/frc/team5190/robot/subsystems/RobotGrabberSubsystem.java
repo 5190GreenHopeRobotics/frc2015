3e853113979b118458578ac8601751739192dd88
Samuel Jin <samfunkit@gmail.com>
2015-01-19 16:37:00 -0500

    added the unprogrammed forklift subsystem

--- src/org/usfirst/frc/team5190/robot/subsystems/RobotGrabberSubsystem.java
+++ src/org/usfirst/frc/team5190/robot/subsystems/RobotGrabberSubsystem.java
@@ -3,7 +3,6 @@ package org.usfirst.frc.team5190.robot.subsystems;
 import org.usfirst.frc.team5190.robot.RobotMap;
 
 
-import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.TalonSRX;
 import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj.command.Subsystem;
