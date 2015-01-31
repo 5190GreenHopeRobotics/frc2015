beb5370b1bd3e926a9c0cbfd95c9756392de224b
Tony Zhang <ghidorah45@gmail.com>
2015-01-31 13:30:52 -0500

    Added documentation to experimental pid arm subsystem

--- src/org/usfirst/frc/team5190/robot/subsystems/ArmextenderPIDSubsystem.java
+++ src/org/usfirst/frc/team5190/robot/subsystems/ArmextenderPIDSubsystem.java
@@ -6,7 +6,7 @@ import edu.wpi.first.wpilibj.TalonSRX;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
 
 /**
- *
+ * experimental subsystem using pid to control the extension of the arm
  */
 
 public class ArmextenderPIDSubsystem extends PIDSubsystem {
@@ -15,6 +15,11 @@ public class ArmextenderPIDSubsystem extends PIDSubsystem {
 	private Encoder armExtenderEncoder = new Encoder(5, 6, false,
 			Encoder.EncodingType.k4X);
 	private TalonSRX armExtenderTalon = new TalonSRX(2);
+
+	/**
+	 * Configures the encoder and enables the default pid controller, creates
+	 * arm extender object
+	 */
 
 	public ArmextenderPIDSubsystem() {
 		super("Armextender", 1.0, 0.0, 0.0);
@@ -27,22 +32,42 @@ public class ArmextenderPIDSubsystem extends PIDSubsystem {
 		armExtenderEncoder.setSamplesToAverage(7);
 	}
 
+	/**
+	 * Gives you current count or angle of the encoder on the extender
+	 * 
+	 * @return
+	 */
 	public double getEncoderangle() {
 		return Math.abs(armExtenderEncoder.get());
 	}
 
+	/**
+	 * Sets the stop point to the current angle
+	 */
 	public void stopExtension() {
 		setSetpoint(getEncoderangle());
 	}
 
+	/**
+	 * Resets the count of the encoder
+	 */
 	public void resetEncoder() {
 		armExtenderEncoder.reset();
 	}
 
+	/**
+	 * gets the direction of the encoder
+	 * 
+	 * @return
+	 */
 	public boolean getEncoderdirection() {
 		return armExtenderEncoder.getDirection();
 	}
 
+	/**
+	 * Gives an encoder output to the pid loop, it will change depending on the
+	 * direction
+	 */
 	protected double returnPIDInput() {
 		// Return your input value for the PID loop
 		// e.g. a sensor, like a potentiometer:
@@ -54,6 +79,9 @@ public class ArmextenderPIDSubsystem extends PIDSubsystem {
 			return -Math.abs(armExtenderEncoder.pidGet());
 	}
 
+	/**
+	 * uses output from the pid loop to control the motor
+	 */
 	protected void usePIDOutput(double output) {
 		// Use output to drive your system, like a motor
 		// e.g. yourMotor.set(output);
