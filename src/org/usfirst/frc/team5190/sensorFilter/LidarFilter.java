9a6f8dc1a44a7fbffa45a8abab507d95e2dab671
Dan Lozier <dan@dloz.net>
2015-02-05 21:25:35 -0500

    null

--- src/org/usfirst/frc/team5190/sensorFilter/LidarFilter.java
+++ src/org/usfirst/frc/team5190/sensorFilter/LidarFilter.java
@@ -13,7 +13,7 @@ public class LidarFilter implements PIDSource, Displayable {
 
 	protected Lidar lidar;
 	protected List<Double> buffer;
-	protected int windowSize = 10;
+	protected int windowSize = 50;
 
 	/**
 	 * set the source for the filter
