8ad8fd635c32d624f865e5a74d6bdd0a2b0e048f
Lu <Lu@LU-LAPTOP>
2015-02-08 11:02:56 -0500

    just checking

--- src/org/usfirst/frc/team5190/robot/subsystems/DriveTrainSubsystem.java
+++ src/org/usfirst/frc/team5190/robot/subsystems/DriveTrainSubsystem.java
@@ -190,7 +190,7 @@ public class DriveTrainSubsystem extends Subsystem implements Displayable {
 				halt();
 				return;
 			}
-			setPower(Robot.oi)
+			setPower(Robot.oi.getSpeed());
 			mDrive.arcadeDrive(stick);
 		}
 	}
