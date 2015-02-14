e9b5bd911340f921b543e21e8aaf308885cbff2c
Shilong Dai <sdai@viperfish.net>
2015-02-14 12:04:14 -0500

    stack two tote

--- src/org/usfirst/frc/team5190/robot/commands/StackedTotesAutonomousCommandGroup.java
+++ src/org/usfirst/frc/team5190/robot/commands/StackedTotesAutonomousCommandGroup.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class StackedTotesAutonomousCommandGroup extends CommandGroup {
 
 	public StackedTotesAutonomousCommandGroup() {
+		// stack two totes in theory
 		addSequential(new DriveToObjectCommand());
 		addSequential(new ProtoAutonomousArmRaiseCommand());
 		addSequential(new DriveToObjectCommand());
