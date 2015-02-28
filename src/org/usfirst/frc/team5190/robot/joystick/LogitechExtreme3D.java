f6a853191f041f76a6928f49647fd44a5abef0d0
alexchung767 <alexchung767@gmail.com>
2015-02-28 11:41:45 -0500

    Constants for ligotech extreme3d

--- src/org/usfirst/frc/team5190/robot/joystick/LogitechExtreme3D.java
+++ src/org/usfirst/frc/team5190/robot/joystick/LogitechExtreme3D.java
@@ -1,5 +1,18 @@
 package org.usfirst.frc.team5190.robot.joystick;
 
-public interface LogitechExtreme3D {
-
+public class LogitechExtreme3D {
+	// Upper buttons on Logitech Flight Stick
+	public static final int TRIGGER = 1;
+	public static final int THUMB_BUTTON = 2;
+	public static final int UPPER_BUTTON_BOTTOM_LEFT = 3;
+	public static final int UPPER_BUTTON_BOTTOM_RIGHT = 4;
+	public static final int UPPER_BUTTON_TOP_LEFT = 5;
+	public static final int UPPER_BUTTON_TOP_RIGHT = 6;
+	// Lower buttons on Logitech Flight stick
+	public static final int BOTTOM_BUTTON_TOP_LEFT = 7;
+	public static final int BOTTOM_BUTTON_TOP_RIGHT = 8;
+	public static final int BOTTOM_BUTTON_CENTER_LEFT = 9;
+	public static final int BOTTOM_BUTTON_CENTER_RIGHT = 10;
+	public static final int BOTTOM_BUTTON_BOTTOM_LEFT = 11;
+	public static final int BOTTOM_BUTTON_BOTTOM_RIGHT = 12;
 }
